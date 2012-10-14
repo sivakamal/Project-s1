490db34beca6147dd5f122bfb76dbc73b3e11f6a
Siva <sivakamal@me.com>
2012-10-15 05:24:36 +0930

    Initial test case

---
+++ Reductionrule1coversTestcase.java
@@ -0,0 +1,27 @@
+package edu.uci.ics.jung;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Reductionrule1coversTestcase extends TestCase {
+	public Reductionrule1coversTestcase(String name)
+	{
+		super (name);
+	}
+	@Test
+	public void test() {
+		Reductionrule1covers z = new Reductionrule1covers();
+		//z.reductioncover();
+
+		//equals(2);
+
+	}
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
